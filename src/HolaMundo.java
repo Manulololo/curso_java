3b74e82f2a9446c99e00041500f2e3701b743726
Tardes <Tardes@GET-06>
2025-07-08 17:55:22 +0200

    Cambio local. Cambio local 2.

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -1,7 +1,8 @@
 
 public class HolaMundo {
 	public static void main(String[]args ) {
-		System.out.println("Hola Getafe 777");
+		System.out.println("Hola Getafe 777 Hola Getafe 888"
+				+ "");
 		
 		//Declarar una variable.
 		int numero;
