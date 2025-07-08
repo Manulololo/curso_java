52f6cdf52125cc5095e686e109e7839053b2934c
Manulololo <manuvalero17@gmail.com>
2025-07-08 17:23:56 +0200

    Update HolaMundo.java desde GitHub

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -1,7 +1,7 @@
 
 public class HolaMundo {
 	public static void main(String[]args ) {
-		System.out.println("Hola Getafe");
+		System.out.println("Hola Getafe 777");
 		
 		//Declarar una variable.
 		int numero;
